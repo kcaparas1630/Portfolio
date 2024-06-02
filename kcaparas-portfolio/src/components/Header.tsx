469bc0307558f36e874eb6da35bdddefa49b970a
kcaparas1630 <kcaparas1630@gmail.com>
2024-06-01 18:46:08 -0700

    installed emotion.sh and added a button

--- kcaparas-portfolio/src/components/Header.tsx
+++ kcaparas-portfolio/src/components/Header.tsx
@@ -1,6 +1,11 @@
+import Button from "../Styled-components/Button";
+
 const Header = () => {
     return(
-        <div><h4><a href='#'>Kcaparas</a></h4></div>
+        <div>
+            <h4><a href='#'>Kcaparas</a></h4>
+            <Button>Click Me!</Button>
+        </div>
     );
 };
 
