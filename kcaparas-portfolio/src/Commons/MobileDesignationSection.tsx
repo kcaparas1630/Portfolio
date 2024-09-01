65230214a8ce08b9afc4a68b198cb91e56df2f15
Kent Caparas <kcaparas1630@gmail.com>
2024-08-31 22:53:37 -0700

    deleted unused imports

--- kcaparas-portfolio/src/Commons/MobileDesignationSection.tsx
+++ kcaparas-portfolio/src/Commons/MobileDesignationSection.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef } from 'react';
+import { FC, useRef } from 'react';
 import { useSpring, motion, useScroll, useTransform } from 'framer-motion';
 import {
   Section,
