9a2cd9a7b41edb91f1e3c1ca3ae7f89314b2872a
kcaparas1630 <kcaparas1630@gmail.com>
2024-09-24 11:05:16 -0700

    hobby section done

--- kcaparas-portfolio/src/Commons/HobbySection.tsx
+++ kcaparas-portfolio/src/Commons/HobbySection.tsx
@@ -1,7 +1,16 @@
 import { FC } from 'react';
 import { useInView } from 'react-intersection-observer';
-import { HobbySection, HeaderContainer } from './Styled-Commons/HobbySection';
+import {
+  HobbySection,
+  HeaderContainer,
+  HobbyContainer,
+  HobbyCard,
+  DescriptionContainer,
+} from './Styled-Commons/HobbySection';
 import ComponentProps from '../Types/ComponentProps';
+import KaraokePic from '../Assets/karaoke.jpg';
+import BadmintonPic from '../Assets/badminton.jpg';
+import CodingPic from '../Assets/coding.jpg';
 
 const HobbySectionComponent: FC<ComponentProps> = ({ isDarkMode }) => {
   const [ref, inView] = useInView({
@@ -10,10 +19,67 @@ const HobbySectionComponent: FC<ComponentProps> = ({ isDarkMode }) => {
   });
 
   return (
-    <HobbySection ref={ref} isDarkMode={isDarkMode}>
+    <HobbySection
+      ref={ref}
+      isDarkMode={isDarkMode}
+    >
       <HeaderContainer inView={inView}>
         <h2>Kent&apos;s Hobbies</h2>
       </HeaderContainer>
+      <HobbyContainer>
+        <HobbyCard
+          isDarkMode={isDarkMode}
+          inView={inView}
+        >
+          <img
+            src={KaraokePic}
+            alt="Karaoke Pic"
+          />
+          <DescriptionContainer>
+            <h4>Karaoke</h4>
+            <p>
+              Singing karaoke is what I love to do for fun. Me and my wife have a karaoke machine at
+              home, that we use when we&apos;re bored. If you&apos;re the same as me, we can always
+              do karaoke for fun!
+            </p>
+          </DescriptionContainer>
+        </HobbyCard>
+        <HobbyCard
+          isDarkMode={isDarkMode}
+          inView={inView}
+        >
+          <img
+            src={CodingPic}
+            alt="Coding Pic"
+          />
+          <DescriptionContainer>
+            <h4>Coding</h4>
+            <p>
+              I&apos;ll code anytime of the day. Wherever or whenever. An idea pops in and I start
+              visualizing it, then implement it. After this portfolio I have a lot of projects to
+              work on so stay tuned!
+            </p>
+          </DescriptionContainer>
+        </HobbyCard>
+        <HobbyCard
+          isDarkMode={isDarkMode}
+          inView={inView}
+        >
+          <img
+            src={BadmintonPic}
+            alt="Badminton Pic"
+          />
+          <DescriptionContainer>
+            <h4>Badminton</h4>
+            <p>
+              Since I sit on a chair all day, this is my method of getting physically active. I
+              loved playing badminton ever since I was in highschool. Apparently what drives me more
+              to playing this is someday beat my athlete wife in this sport which she&apos;s a
+              professional.
+            </p>
+          </DescriptionContainer>
+        </HobbyCard>
+      </HobbyContainer>
     </HobbySection>
   );
 };
--- kcaparas-portfolio/src/Commons/Styled-Commons/HobbySection.ts
+++ kcaparas-portfolio/src/Commons/Styled-Commons/HobbySection.ts
@@ -12,6 +12,7 @@ const HobbySection = styled(motion.section)<SectionProps>`
   color: ${(props) => (props.isDarkMode ? 'white' : '#1B1212')};
   position: relative;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 
@@ -58,5 +59,60 @@ const HeaderContainer = styled(motion.div)<SectionProps>`
     }
   }
 `;
+const HobbyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+  justify-content: center;
 
-export { HobbySection, HeaderContainer };
+  width: 100vw;
+  height: 100%;
+
+  @media (min-width: 1024px) {
+    flex-direction: row;
+  }
+`;
+const HobbyCard = styled.div<SectionProps>`
+  label: HobbyCard;
+  border-radius: 5px;
+  width: 90%;
+  padding-bottom: 24px;
+  background-color: ${(props) => (props.isDarkMode ? '#1C225F' : '#131842')};
+  color: white;
+  box-shadow: 0 4px 8px 0 #e68369;
+  display: flex;
+  flex-direction: column;
+  padding-top: 24px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: scale-down;
+    border-radius: 5px 5px 0 0;
+  }
+  h4 {
+    font-size: 1.25rem;
+    font-weight: 700;
+  }
+  p {
+    font-size: 1rem;
+    opacity: 0.8;
+  }
+  transform: translateY(${(props) => (props.inView ? '0' : '100%')});
+  opacity: ${(props) => (props.inView ? '1' : '0')};
+  transition:
+    transform 1s ease,
+    box-shadow 0.3s ease-in-out,
+    opacity 1s ease;
+
+  @media (min-width: 1024px) {
+    width: 20%;
+    max-height: 500px;
+  }
+`;
+
+const DescriptionContainer = styled.div`
+  padding: 2px 16px;
+`;
+
+export { HobbySection, HeaderContainer, HobbyContainer, HobbyCard, DescriptionContainer };
